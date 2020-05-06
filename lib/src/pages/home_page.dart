ed2dea690cabd4cecfcf5e1b9645b83a45ec81df
Javier Quinteros <javier@nora.cl>
2020-05-06 16:42:05 -0400

    Added HomePage and Scaffold.

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffEBE254),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
